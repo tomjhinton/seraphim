c0c2cd716bf0cabe59ec67c7cf595ee2e2a22181
Tom Hinton <tomjhinton@gmail.com>
2020-06-04 20:09:17 +0100

    app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from pony.orm import Database
+
+from config.environment import db_uri
+
+
+app = Flask(__name__, static_folder='dist')
+db = Database()
+db.bind('postgres', db_uri)
+
+
+# pylint: disable=W0611,C0413
+from config import routes
+db.generate_mapping(create_tables=True)
+
+@app.errorhandler(404)
+def not_found(_error):
+    return jsonify({'message': 'Not found'}), 404
